cf3ff3b113bf1bfcaed9bcacbffa6605bae99e7c
Tomas Votruba <tomas.vot@gmail.com>
2023-02-24 09:06:42 +0000

    improve easy cI

---
+++ easy-ci.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCI\Config\EasyCIConfig;
+
+return static function (EasyCIConfig $easyCIConfig): void {
+    $easyCIConfig->typesToSkip([
+        \Illuminate\Contracts\Console\Kernel::class,
+    ]);
+};
--- src/Kernel/PunchcardKernel.php
+++ src/Kernel/PunchcardKernel.php
@@ -8,15 +8,4 @@ use Illuminate\Foundation\Console\Kernel;
 
 final class PunchcardKernel extends Kernel
 {
-    ///**
-    // * @param InputInterface $input
-    // * @param OutputInterface $output
-    // */
-    //public function handle($input, $output = null): int
-    //{
-    //    $this->app->instance(InputInterface::class, $input);
-    //    $this->app->instance(OutputInterface::class, $output);
-    //
-    //    return parent::handle($input, $output);
-    //}
 }
