67ce01df4ce27c7e30da4ef8479dbfaabcab2bba
GitHub Action <action@github.com>
2023-03-31 14:12:01 +0000

    release Generated Configs

---
+++ generated/MailConfig.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace TomasVotruba\PunchCard;
+
+class MailConfig implements \Illuminate\Contracts\Support\Arrayable
+{
+    private ?string $default = null;
+
+    /**
+     * @var string[][][]
+     */
+    private array $mailers = [];
+
+    /**
+     * @var string[]
+     */
+    private array $from = [];
+
+    /**
+     * @var string[][]
+     */
+    private array $markdown = [];
+
+    public static function make(): self
+    {
+        return new self();
+    }
+
+    public function defaults(): self
+    {
+        $this->default(env('MAIL_MAILER', 'smtp'));
+        $this->mailers([
+            'smtp' => [
+                'transport' => 'smtp',
+                'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+                'port' => env('MAIL_PORT', 587),
+                'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+                'username' => env('MAIL_USERNAME'),
+                'password' => env('MAIL_PASSWORD'),
+                'timeout' => \null,
+                'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            ],
+            'ses' => [
+                'transport' => 'ses',
+            ],
+            'mailgun' => [
+                'transport' => 'mailgun',
+            ],
+            'postmark' => [
+                'transport' => 'postmark',
+            ],
+            'sendmail' => [
+                'transport' => 'sendmail',
+                'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
+            ],
+            'log' => [
+                'transport' => 'log',
+                'channel' => env('MAIL_LOG_CHANNEL'),
+            ],
+            'array' => [
+                'transport' => 'array',
+            ],
+            'failover' => [
+                'transport' => 'failover',
+                'mailers' => [
+                    'smtp',
+                    'log',
+                ],
+            ],
+        ]);
+        $this->from([
+            'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+            'name' => env('MAIL_FROM_NAME', 'Example'),
+        ]);
+        $this->markdown([
+            'theme' => 'default',
+            'paths' => [
+                resource_path('views/vendor/mail'),
+            ],
+        ]);
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Mailer
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default mailer that is used to send any email
+    | messages sent by your application. Alternative mailers may be setup
+    | and used as needed; however, this mailer will be used by default.
+    |
+    */
+    public function default(string $default): self
+    {
+        $this->default = $default;
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mailer Configurations
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure all of the mailers used by your application plus
+    | their respective settings. Several examples have been configured for
+    | you and you are free to add your own as your application requires.
+    |
+    | Laravel supports a variety of mail "transport" drivers to be used while
+    | sending an e-mail. You will specify which one you are using for your
+    | mailers below. You are free to add additional mailers as required.
+    |
+    | Supported: "smtp", "sendmail", "mailgun", "ses", "ses-v2",
+    |            "postmark", "log", "array", "failover"
+    |
+    */
+    /**
+     * @param string[][][] $mailers
+     */
+    public function mailers(array $mailers): self
+    {
+        $this->mailers = $mailers;
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global "From" Address
+    |--------------------------------------------------------------------------
+    |
+    | You may wish for all e-mails sent by your application to be sent from
+    | the same address. Here, you may specify a name and address that is
+    | used globally for all e-mails that are sent by your application.
+    |
+    */
+    /**
+     * @param string[] $from
+     */
+    public function from(array $from): self
+    {
+        $this->from = $from;
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Markdown Mail Settings
+    |--------------------------------------------------------------------------
+    |
+    | If you are using Markdown based email rendering, you may configure your
+    | theme and component paths here, allowing you to customize the design
+    | of the emails. Or, you may simply stick with the Laravel defaults!
+    |
+    */
+    /**
+     * @param string[][] $markdown
+     */
+    public function markdown(array $markdown): self
+    {
+        $this->markdown = $markdown;
+        return $this;
+    }
+
+    /**
+     * @return array<string, mixed[]>
+     */
+    public function toArray(): array
+    {
+        return [
+            'default' => $this->default,
+            'mailers' => $this->mailers,
+            'from' => $this->from,
+            'markdown' => $this->markdown,
+        ];
+    }
+}
---
+++ generated/SanctumConfig.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace TomasVotruba\PunchCard;
+
+class SanctumConfig implements \Illuminate\Contracts\Support\Arrayable
+{
+    /**
+     * @var string[]
+     */
+    private array $stateful = [];
+
+    /**
+     * @var string[]
+     */
+    private array $guard = [];
+
+    private ?int $expiration = null;
+
+    /**
+     * @var array<class-string<\App\Http\Middleware\EncryptCookies>>
+     */
+    private array $middleware = [];
+
+    public static function make(): self
+    {
+        return new self();
+    }
+
+    public function defaults(): self
+    {
+        $this->stateful(explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf('%s%s', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1', \Laravel\Sanctum\Sanctum::currentApplicationUrlWithPort()))));
+        $this->guard([
+            'web',
+        ]);
+        $this->expiration(\null);
+        $this->middleware([
+            'verify_csrf_token' => \App\Http\Middleware\VerifyCsrfToken::class,
+            'encrypt_cookies' => \App\Http\Middleware\EncryptCookies::class,
+        ]);
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Stateful Domains
+    |--------------------------------------------------------------------------
+    |
+    | Requests from the following domains / hosts will receive stateful API
+    | authentication cookies. Typically, these should include your local
+    | and production domains which access your API via a frontend SPA.
+    |
+    */
+    /**
+     * @param string[] $stateful
+     */
+    public function stateful(array $stateful): self
+    {
+        $this->stateful = $stateful;
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sanctum Guards
+    |--------------------------------------------------------------------------
+    |
+    | This array contains the authentication guards that will be checked when
+    | Sanctum is trying to authenticate a request. If none of these guards
+    | are able to authenticate the request, Sanctum will use the bearer
+    | token that's present on an incoming request for authentication.
+    |
+    */
+    /**
+     * @param string[] $guard
+     */
+    public function guard(array $guard): self
+    {
+        $this->guard = $guard;
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Expiration Minutes
+    |--------------------------------------------------------------------------
+    |
+    | This value controls the number of minutes until an issued token will be
+    | considered expired. If this value is null, personal access tokens do
+    | not expire. This won't tweak the lifetime of first-party sessions.
+    |
+    */
+    public function expiration(?int $expiration): self
+    {
+        $this->expiration = $expiration;
+        return $this;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sanctum Middleware
+    |--------------------------------------------------------------------------
+    |
+    | When authenticating your first-party SPA with Sanctum you may need to
+    | customize some of the middleware Sanctum uses while processing the
+    | request. You may change the middleware listed below as required.
+    |
+    */
+    /**
+     * @param array<class-string<\App\Http\Middleware\EncryptCookies>> $middleware
+     */
+    public function middleware(array $middleware): self
+    {
+        $this->middleware = $middleware;
+        return $this;
+    }
+
+    /**
+     * @return array<string, mixed[]>
+     */
+    public function toArray(): array
+    {
+        return [
+            'stateful' => $this->stateful,
+            'guard' => $this->guard,
+            'expiration' => $this->expiration,
+            'middleware' => $this->middleware,
+        ];
+    }
+}
